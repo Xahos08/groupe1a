8196630f021b6e0e726666e185ee210fa5d08d24
Xahos08 <Whcilboer1>
2016-01-19 15:27:13 +0100

    Petite modife de routine

---
+++ groupe1a/src/main/java/com/helha/groupe1a/classesEJB/GestionUserEJB.java
@@ -0,0 +1,20 @@
+package com.helha.groupe1a.classesEJB;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+import com.helha.groupe1a.entities.User;
+import com.helha.groupe1a.interfacesEJB.GestionProjectEJBRemote;
+import com.helha.groupe1a.interfacesEJB.GestionUserEJBRemote;
+
+@Stateless
+public class GestionUserEJB implements GestionUserEJBRemote{
+
+	@EJB
+	DAOUser bean;
+	
+	@Override
+	public void add(User u) {
+		bean.add(u);
+	}
+}
