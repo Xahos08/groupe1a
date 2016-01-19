4be6e299b1dd8947b22b089e4c1b4cac34d9d146
Romain V. <the_bresilien@hotmail.fr>
2016-01-19 12:27:48 +0100

    > Adrien modif merged

---
+++ groupe1a/src/main/java/com/helha/groupe1a/services/RestProject.java
@@ -0,0 +1,46 @@
+package com.helha.groupe1a.services;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import com.helha.groupe1a.entities.Project;
+import com.helha.groupe1a.test.DAO;
+
+
+@Path("/api")
+public class RestProject {
+	@EJB
+	DAO bean;
+	
+	@Path("/selectAll")
+	@GET
+	public List<Project> selectAll() {
+		return bean.getProjectsList();
+	}
+	@Path("/find/{name}")
+	@GET
+	public List<Project> find(@PathParam("name")String name) {
+		return bean.getProject(name);
+	}
+	
+	@Path("/delete")
+	@DELETE
+	public Project delete(Project p) {
+		bean.deleteProject(p);
+		return p;
+	}
+	
+	@Path("/add")
+	@POST
+	public Project add(Project p) {
+		bean.addProject(p);
+		return p;
+	}
+
+}
