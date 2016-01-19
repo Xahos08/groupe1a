4c65bb7e9cf99de19891ab8949803a2fa393859a
Xahos08 <Whcilboer1>
2016-01-19 15:06:45 +0100

    Modification et separer le DAO en DAOUser et DAOProject Creation des services, interfacesEJB pour Project

--- groupe1a/src/main/java/com/helha/groupe1a/controller/ProjectController.java
+++ groupe1a/src/main/java/com/helha/groupe1a/controller/ProjectController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.helha.groupe1a.controller;
 
 import java.util.Date;
@@ -25,8 +24,8 @@ public class ProjectController {
 	private String name;
 	private double amountExpected;
 	private double amountEarned;
-	private Date dateBegin;
-	private Date dateEnd;
+	private String dateBegin;
+	private String dateEnd;
 	
 	public ProjectController(){
 		
@@ -84,125 +83,16 @@ public class ProjectController {
 	public void setAmountEarned(double amountEarned) {
 		this.amountEarned = amountEarned;
 	}
-	public Date getDateBegin() {
+	public String getDateBegin() {
 		return dateBegin;
 	}
-	public void setDateBegin(Date dateBegin) {
+	public void setDateBegin(String dateBegin) {
 		this.dateBegin = dateBegin;
 	}
-	public Date getDateEnd() {
+	public String getDateEnd() {
 		return dateEnd;
 	}
-	public void setDateEnd(Date dateEnd) {
+	public void setDateEnd(String dateEnd) {
 		this.dateEnd = dateEnd;
 	}
-	
-	
-=======
-package com.helha.groupe1a.controller;
-
-import java.util.Date;
-import java.util.List;
-
-import javax.ejb.EJB;
-import javax.enterprise.context.RequestScoped;
-import javax.faces.bean.ManagedBean;
-import javax.inject.Named;
-
-import com.helha.groupe1a.entities.Project;
-import com.helha.groupe1a.test.DAO;
-
-@Named
-@ManagedBean(name = "ProjectController")
-@RequestScoped
-public class ProjectController {
-	@EJB
-	private DAO bean;
-	
-	private int id;
-	
-	private String category;
-	private String name;
-	private double amountExpected;
-	private double amountEarned;
-	private Date dateBegin;
-	private Date dateEnd;
-	
-/*	public ProjectController(){
-		
-	}
-	public List<Project> doSelectAll(){
-		return bean.selectAllProject();
-	}
-	public List<Project> doFind() {
-		return bean.getProject(name);
-	}
-	
-	public void doAdd() {
-		bean.addProject(new Project(category,name,amountExpected,amountEarned,dateBegin,dateEnd));
-		name = "";
-		category = "";
-		dateBegin = null;
-		dateEnd=null;
-		amountExpected=0;
-		amountEarned=0;
-		id=0;
-	}
-	
-	public void doDelete() {
-		bean.deleteProject(new Project(category,name,amountExpected,amountEarned,dateBegin,dateEnd));
-		name = "";
-		category = "";
-		dateBegin = null;
-		dateEnd=null;
-		amountExpected=0;
-		amountEarned=0;
-		id = 0;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public void setCategory(String category) {
-		this.category = category;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public double getAmountExpected() {
-		return amountExpected;
-	}
-	public void setAmountExpected(double amountExpected) {
-		this.amountExpected = amountExpected;
-	}
-	public double getAmountEarned() {
-		return amountEarned;
-	}
-	public void setAmountEarned(double amountEarned) {
-		this.amountEarned = amountEarned;
-	}
-	public Date getDateBegin() {
-		return dateBegin;
-	}
-	public void setDateBegin(Date dateBegin) {
-		this.dateBegin = dateBegin;
-	}
-	public Date getDateEnd() {
-		return dateEnd;
-	}
-	public void setDateEnd(Date dateEnd) {
-		this.dateEnd = dateEnd;
-	}
-	
-	*/
->>>>>>> branch 'master' of https://github.com/Xahos08/groupe1a.git
 }
--- groupe1a/src/main/java/com/helha/groupe1a/services/RestProject.java
+++ groupe1a/src/main/java/com/helha/groupe1a/services/RestProject.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.helha.groupe1a.services;
 
 import java.util.List;
@@ -43,52 +42,4 @@ public class RestProject {
 		bean.add(p);
 		return p;
 	}
-
-=======
-package com.helha.groupe1a.services;
-
-import java.util.List;
-
-import javax.ejb.EJB;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-
-import com.helha.groupe1a.entities.Project;
-import com.helha.groupe1a.test.DAO;
-
-
-@Path("/api")
-public class RestProject {
-	/*@EJB
-	DAO bean;
-	
-	@Path("/selectAll")
-	@GET
-	public List<Project> selectAll() {
-		return bean.selectAllProject();
-	}
-	@Path("/find/{name}")
-	@GET
-	public List<Project> find(@PathParam("name")String name) {
-		return bean.getProject(name);
-	}
-	
-	@Path("/delete")
-	@DELETE
-	public Project delete(Project p) {
-		bean.deleteProject(p);
-		return p;
-	}
-	
-	@Path("/add")
-	@POST
-	public Project add(Project p) {
-		bean.addProject(p);
-		return p;
-	}
-*/
->>>>>>> branch 'master' of https://github.com/Xahos08/groupe1a.git
 }
