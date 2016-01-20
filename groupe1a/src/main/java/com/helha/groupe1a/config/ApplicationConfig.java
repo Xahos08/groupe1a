23c8a06768720abcdce5e3eb61dc76a2f2e528d4
Xahos08 <Whcilboer1>
2016-01-20 09:15:52 +0100

    ajout des pages about et startProject2 ApplicationConfig non fonctionnel, mis en commentaire

---
+++ groupe1a/src/main/java/com/helha/groupe1a/config/ApplicationConfig.java
@@ -0,0 +1,14 @@
+package com.helha.groupe1a.config;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+/*@ApplicationPath("services")
+public class ApplicationConfig extends ResourceConfig {
+
+	public ApplicationConfig() {
+		packages("com.helha.groupe1a.services");
+	}
+	
+}*/
